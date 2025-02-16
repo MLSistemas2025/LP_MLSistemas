5bbd68409ab4f0e33ce76e4ae2c19923b273070e
Paulo Henrique de Morais <Mlsistemas2025@gmail.com>
2025-02-15 21:17:33 -0300

    Alteracoes
    Alterações no lugar do email, agora é feito pelo javascript não mais no index.html;
    removido logos da empresa antiga;
    Ajustado css para botao do dialogo(inacabado)

--- LP_M&L_Sistemas/LandingPage_M&LSistemas/src/js/script.js
+++ LP_M&L_Sistemas/LandingPage_M&LSistemas/src/js/script.js
@@ -1,6 +1,29 @@
 var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
+const button = document.getElementById("ButtonModal");
+const modal = document.querySelector("dialog");
+const buttonClose = document.querySelector("dialog button");
+
+button.onclick = function() {
+  modal.showModal();
+}
+
+buttonClose.onclick = function() {
+  modal.close();
+}
+
+//busca o elemento pelo id dele no index.html
+const form = document.getElementById('contactForm');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); 
+        const formData = new FormData(form); 
+        fetch('https://formsubmit.co/mlsistemas.contato@gmail.com', { 
+            method: 'POST',
+            body: formData
+        })
+    });
+
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     themeToggleLightIcon.classList.remove('hidden');
